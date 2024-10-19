1d66fa4e27cf16f98f01bad68a3261e5c05f2024
tymur-lvov <pmtl753@gmail.com>
2024-10-19 22:42:38 +0300

    Fix objectDeepClone

--- objectDeepClone.js
+++ objectDeepClone.js
@@ -1,8 +1,8 @@
-const object = { a: 1, b: 2, c: { d: 3, e: 4 }, f: 5 };
+const object = { a: 1, b: 2, c: { d: 3, e: 4 }, f: null };
 
 const objectDeepClone = (obj) => {
   return Object.keys(obj).reduce((acc, el) => {
-    if (typeof obj[el] === 'object' && typeof obj[el] !== null) {
+    if (typeof obj[el] === 'object' && obj[el] !== null) {
       acc[el] = objectDeepClone(obj[el]);
     } else {
       acc[el] = obj[el];
